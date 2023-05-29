14fe289c13427e87239b64517b6bb4e89576aa86
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-30 00:27:12 +0300

    #3

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-url = 'https://www.example.com'
+url = "https://www.example.com"
 response = requests.get(url)
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-tables = soup.find_all('table')
-for table in tables:
-    print(table)
-    print('\n')
+html_content = response.text
+soup = BeautifulSoup(html_content, "html.parser")
+example_elements = soup.find_all(class_="example-class")
+for element in example_elements:
+    print(element)
