e5ced235c0d2958b2c680cfd4ecda5cb4844d010
dimakuchinskyi <kuci.ia77@gmail.com>
2023-05-30 00:34:11 +0300

    #4

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from bs4 import BeautifulSoup
 url = "https://www.example.com"
 response = requests.get(url)
 html_content = response.text
-soup = BeautifulSoup(html_content, "html.parser")
-example_elements = soup.find_all(class_="example-class")
-for element in example_elements:
-    print(element)
+soup = BeautifulSoup(html_content, 'html.parser')
+h1_element = soup.find('h1')
+if h1_element:
+    print("Заголовок (тег <h1>):")
+    print("Текст: ", h1_element.text)
+    print("Атрибути: ", h1_element.attrs)
+else:
+    print("Заголовок (тег <h1>) не знайдено на сторінці.")
